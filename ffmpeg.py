0e6fe3cb3de027b12d787b2c456db68b13f94243
Jon Reades <jon@reades.com>
2025-09-05 17:28:50 +0100

    A lot more is dynamically rendered now though I find the y-position stuff really confusing.

--- ffmpeg.py
+++ ffmpeg.py
@@ -8,12 +8,106 @@ class textblock:
     style = str()
 
     def __init__(self, txt:str, halign:str="center", valign:str="middle", size:int=12, font:str="Hanken Grotesk", style:str="Regular"):
-        self.lines = list(txt.split("\\n"))
+        self.lines = txt.split("\n") if "\n" in txt else [txt]
         self.size = size
         self.h_align = halign
         self.v_align = valign
         self.font = font
         self.style = style
+        self.position = position(0, 0)
+
+class position:
+
+    def __init__(self, x:float, y:float):
+
+        self.x = x
+        self.y = y
+    
+    def get_x(self):
+        raise NotImplementedError(
+            "Subclasses must implement abstract method"
+        )
+
+    def get_y(self):
+        raise NotImplementedError(
+            "Subclasses must implement abstract method"
+        )
+
+    def __repr__(self):
+        return f'x={self.get_x()}:y={self.get_y()}'
+
+class tposition(position):
+
+    # Notice!!!! Bottom-left is (0,0) in ffmpeg
+    # But not seemingly for drawtext???
+    left   = float(0)
+    top    = float(0)
+
+    center = '(main_w-text_w)/2'
+    middle = '(main_h-text_h)/2'
+
+    right  = '(main_w-text_w)'
+    bottom = '(main_h-text_h)'
+
+    def get_x(self):
+        if self.x == 0:
+            return(f"{self.left}")
+        elif self.x == 0.5:
+            return(f"{self.center}")
+        elif self.x == 1:
+            return(f"{self.right}")
+        else:
+            return(f"(main_w*{self.x:0.2f})")
+
+    def get_y(self):
+        if self.y == float(0):
+            return(f"{self.bottom}")
+        elif self.y == float(0.5):
+            return(f"{self.middle}")
+        elif self.y == float(1):
+            return(f"{self.top}")
+        else:
+            return(f"(main_h*{self.y:0.4f})") # -(text_h/2)
+
+    def __init__(self, x:float, y:float):
+        super().__init__(x, y)
+
+class iposition(position):
+
+    # Notice!!!! Bottom-left is (0,0) in ffmpeg
+    # But not seemingly for drawtext???
+    left   = float(0)
+    bottom = float(0)
+
+    center = '(main_w-overlay_w)/2'
+    middle = '(main_h-overlay_h)/2'
+
+    right = '(main_w-overlay_w)'
+    top   = '(main_h-overlay_h)'
+
+    def get_x(self) -> str:
+        if self.x == 0:
+            return(f"{self.left}")
+        elif self.x == 0.5:
+            return(f"{self.center}")
+        elif self.x == 1:
+            return(f"{self.right}")
+        else:
+            return(f"(main_w*{self.x:0.2f})")
+
+    def get_y(self) -> str:
+        if self.y == 0:
+            return(f"{self.bottom}")
+        elif self.y == 0.5:
+            return(f"{self.middle}")
+        elif self.y == 1:
+            return(f"{self.top}")
+        else:
+            #return(f"(h+overlay_h/2)*{self.y:0.2f}")
+            return(f"(main_h*{self.y:0.4f})") # -(overlay_h/2)
+
+    def __init__(self, x:float, y:float):
+        super().__init__(x, y)
 
 class overlay: 
     
@@ -104,7 +198,7 @@ class text:
     fader = None
 
     def __init__(self, text:str, x:str, y:str, size:int, color:str, 
-                 font:str, style:str="Regular"):
+                 font:str, style:str="Regular", halign:str="", valign:str=""):
         self.label = text
         self.x = x
         self.y = y
@@ -112,6 +206,8 @@ class text:
         self.font = font
         self.color = color
         self.style = style
+        self.halign = halign if halign != "" else None
+        self.valign = valign if valign != "" else None
 
     def add_fader(self, f:txt_fade):
         self.fader = f
@@ -119,4 +215,5 @@ class text:
     def __repr__(self):
         return f"drawtext=fontfile='{self.font}\\:style={self.style}':text='{self.label}':" \
              + f"fontcolor={self.color}:fontsize={self.size}:x={self.x}:y={self.y}" \
+             + (f":text_align={'L' if self.halign == 'left' else 'R'}+{'B' if self.valign == 'bottom' else 'T'}:fix_bounds=true" if self.halign is not None else "") \
              + (f":alpha='{str(self.fader)}'," if self.fader else "")
--- intro.py
+++ intro.py
@@ -34,9 +34,15 @@ else:
     lfo  = stfo + fil + 1.0 # lecture fade out
     run_len = lfo + fol
 
+print(f"Start fade in at {stfi:0.2f} for {fil:0.2f}s")
+print(f"Start fade out at {stfo:0.2f} for {fol:0.2f}s")
+print(f"Lecture fade in at {lfi:0.2f}s")
+print(f"Lecture fade out at {lfo:0.2f}s")
+print(f"Running length at {run_len:0.2f}s")
+
 # Set up scene
 intro = intro(run_len)
-intro.color = "#574d62" # dark purple
+intro.color = "42324a" # "#574d62" # dark purple
 
 print(f"+ Video details:\n\tStart fade in at {stfi:0.2f} and fade in for {fil:0.2f}\n\tFully visible from {(stfi+fil):0.2f} to {stfo:0.2f} ({stfo-(stfi+fil):0.2f}s)\n\tStart fade out at {stfo:0.2f} and fade out for {fol:0.2f}.")
 print(f"+ Talk details:\n\tStart fade in at {lfi:0.2f} and fade in for {fil:0.2f}\n\tFully visible from {(lfi+fil):0.2f} to {lfo:0.2f} ({lfo-(lfi+fil):0.2f}s)\n\tStart fade out at {lfo:0.2f} and fade out for {fol:0.2f}.")
@@ -44,26 +50,26 @@ print(f"+ Running length at {run_len:0.2f}.\n")
 
 # Image overlays
 bgimg = overlay('0', '0', True)
-ccby  = overlay('main_w/2-overlay_w/2', 'main_h/2+150', True)
-logo  = overlay('main_w/2-overlay_w/2', 'main_h/2+overlay_h*0.175', True)
-
-# Background fading
-bg_fade_in = img_fade(True, fil, stfi)
-bg_fade_out = img_fade(False, fol, stfo)
+ccby  = overlay(ccby_position.get_x(), ccby_position.get_y(), True)
+logo  = overlay(logo_position.get_x(), logo_position.get_y(), True)
 
 # Course name fading
 course_fade_in = txt_fade(True, fil, stfi)
 course_fade_out = txt_fade(False, fol, stfo)
 course_x_fade = cross_fader(course_fade_in, course_fade_out)
 
-# Logo fading
-fi = img_fade(True, fil, stfi)
-fo = img_fade(False, fol, stfo)
-
 # Lecture title fading
 title_fade_in  = txt_fade(True, fil, lfi)
 title_fade_out = txt_fade(False, fol, lfo)
 xt_fade = cross_fader(title_fade_in, title_fade_out)
+
+# Logo fading
+fi = img_fade(True, fil, stfi)
+fo = img_fade(False, fol, stfo)
+
+# Background fading
+bi = img_fade(True, fil, stfi)
+bo = img_fade(False, fol, lfo)
 
 # Course title text
 titles = []
@@ -78,7 +84,10 @@ for i, txt in enumerate(title.lines):
     else:
         y = ""
     print(f"Course title line {i} '{txt}' will be positioned at y={y}.")
-    t = text(txt, '(w-text_w)/2', f'(h/2){y}', title.size, color="ffffff", font=title.font, style=title.style)
+    print(f"    Position: x={title.position.get_x()}, y={title.position.get_y()}{y}")
+    t = text(txt, title.position.get_x(), f"{title.position.get_y()}{y}",
+             title.size, color=fontcolor_light, font=title.font, style=title.style,
+             halign=title.h_align, valign=title.v_align)
     t.add_fader(course_x_fade)
     titles.append(t)
 
@@ -95,25 +104,32 @@ for i, txt in enumerate(lecture.lines):
     else:
         y = ""
     print(f"Lecture title line {i} '{txt}' will be positioned at y={y}.")
-    t = text(txt.replace(':','\\:'), '(w-text_w)/2', f'(h/2){y}', lecture.size, color="ffffff", font=lecture.font, style=lecture.style)
+    print(f"    Position: x={lecture.position.get_x()}, y={lecture.position.get_y()}{y}")
+    t = text(txt.replace(':','\\:'), lecture.position.get_x(), f"{lecture.position.get_y()}{y}", 
+             lecture.size, color=fontcolor_light, font=lecture.font, style=lecture.style,
+             halign=lecture.h_align, valign=lecture.v_align)
     t.add_fader(xt_fade)
     titles.append(t)
 
 # Year for talk
 y = f"-({round(middle_index * lecture.size * leading + lecture.size * 0.75 * leading)})"
-yr = text(f"{year.lines[0]}/{int(year.lines[0])+1}", '(w-text_w)/2', f'(h/2{y})', year.size, color="ffffff", font=year.font, style=year.style)
+yr = text(f"{year.lines[0]}/{int(year.lines[0])+1}", year.position.get_x(), year.position.get_y(), 
+          year.size, color=fontcolor_light, font=year.font, style=year.style, 
+          halign=year.h_align, valign=year.v_align)
 yr.add_fader(xt_fade)
 
 # Copyright line
 y = f"+({round((middle_index + 1) * lecture.size * leading)})"
-copy = text(f"by {author.lines[0]}", '(w-text_w)/2', f'(h/2{y})', author.size, color="ffffff", font=author.font, style=author.style)
+copy = text(f"by {author.lines[0]}", author.position.get_x(), author.position.get_y(), 
+            author.size, color=fontcolor_light, font=author.font, style=author.style,
+            halign=author.h_align, valign=author.v_align)
 copy.add_fader(xt_fade)
 
 params = {
     'bg': str(intro),
-    'img': f'[0:v]scale=1280:-1,colorchannelmixer=aa=0.175',
-    'logo': f'[1:v]{fi}, {fo}',
-    'ccby': f'[2:v]{fi}, {fo}'
+    'img': f'[0:v]scale=1280:-1,colorchannelmixer=aa=0.175, {bi}, {bo}',
+    'logo': f'[1:v]scale=52:-1, {fi}, {fo}',
+    'ccby': f'[2:v]{fi}, {bo}'
 }
 
 cmd = ''
