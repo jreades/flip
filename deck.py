12bce4f0e2d50c45e35ee6e0f795071f3b91d902
Jon Reades <jon@reades.com>
2025-09-09 23:23:50 +0100

    Updated to use TOML framework.

--- deck.py
+++ deck.py
@@ -1,44 +1,69 @@
 ######################
 # Take a lecture and extract it to a set of PNG files. 
 ######################
-import argparse
+import argparse, tomllib
 from subprocess import call
-import os, re, glob
+import re, glob
+from pathlib import Path
 
-scrn_size   = "1280x720"
-scrn_format = "png"
-pause       = 500 # Applied to both --pause and --load-pause
-
-ppath = os.path.join(os.path.expanduser("~"),"anaconda3","envs","sds","bin")
+DEBUG = False
 
 parser = argparse.ArgumentParser(
                     prog='extract_deck.py',
                     description='Generates a set of PNGs from a Reveal.js lecture. Requires node.js and decktape to be installed.',
                     epilog='For example: `python ffmpeg/extract.py -t 3.4-Functions -s https://jreades.github.io/fsds/lectures`')
-parser.add_argument('-t', '--talk', type=str, help="The folder name with the talk.")
-parser.add_argument('-s', '--server', type=str, help="Where to access the Reveal.js slides", default='http://localhost:4200/lectures')
+parser.add_argument('-p', '--project', type=str, help="Path to the project.toml configuration file.", default='project.toml')
+parser.add_argument('-d', '--defaults', type=str, help="Path to the defaults.toml configuration file.", default='defaults.toml')
+parser.add_argument('-l', '--lesson', type=str, help="Name of the lesson in the project.toml configuration file.", default='1')
 
 args = parser.parse_args()
 
-# Strip off the HTML suffix (or any other suffix)
-if re.search(r'\.\w{2,4}$', args.talk):
-    args.talk = re.sub(r'\.\w{2,4}$','',args.talk)
+if args.defaults != None and Path(args.defaults).exists():
+    with open(args.defaults, 'rb') as f:
+        conf = tomllib.load(f)
+else:
+    conf = {}
 
-args.output = os.path.join('_export',args.talk)
+if args.project != None and Path(args.project).exists():
+    with open(args.project, 'rb') as f:
+        proj = tomllib.load(f)
+else:
+    proj = {}
+
+# Merge project settings into conf
+for k,v in proj.items():
+    if k not in conf:
+        conf[k] = v
+
+parent = Path(conf['outputs']['slides'])
+parent.mkdir(parents=True, exist_ok=True)
+
+args.output = parent / conf['lessons'][args.lesson]['track'].strip()
+
+scrn_size   = conf['project'].get('size', '1280x720')
+scrn_format = conf['project'].get('format', 'png')
+pause       = conf['project'].get('pause', 500)
 
 # Create the folder for storing the files
-if not os.path.exists(args.output):
-    os.makedirs(args.output, exist_ok=True)
+if not args.output.exists():
+    args.output.mkdir(parents=True, exist_ok=True)
     print(f"+ Created {args.output}")
 else:
     print(f"+ Found {args.output}")
-    files = glob.glob(os.path.join(args.output, "*.png"))
+    files = glob.glob(str(args.output / "*.m4a"))
     if len(files) > 0:
         print(f"  - Emptying directory of already-rendered output")
         for f in files:
-            os.remove(f)
+            Path(f).unlink()
 
 print(f"+ Preparing to extract screenshots.")
+
+server_path = f"{conf['project']['server']}/" \
+                f"{conf['lessons'][str(args.lesson)]['week']}." \
+                f"{conf['lessons'][str(args.lesson)]['sequence']}-" \
+                f"{re.sub(r'[^a-zA-Z0-9]+','_',conf['lessons'][str(args.lesson)]['track'].strip())}" \
+                ".html"
+print(server_path)
 
 # And extract the slides as PNGs to the folder
 cmd = ''
@@ -46,11 +71,12 @@ cmd += f'decktape \\\n'
 cmd += f'  --screenshots --screenshots-directory {args.output} \\\n'
 cmd += f'  --size {scrn_size} --screenshots-size {scrn_size} --screenshots-format {scrn_format} \\\n'
 cmd += f'  --pause {pause} --load-pause {pause} --headless true \\\n'
-cmd += f'  {os.path.join(args.server, args.talk+'.html')} {args.talk + '.pdf'}'
+cmd += f"  {server_path} {conf['lessons'][str(args.lesson)]['track'].strip() + '.pdf'}"
 
-#print(f"  -i: {cmd}")
+if re.DEBUG:
+    print(f"  -i: {cmd}")
 call(cmd, shell=True)
 
-print(f"+++ PNGs for {args.talk} generated +++")
+print(f"+ PNGs for {conf['lessons'][str(args.lesson)]['track'].strip()} generated +++")
 
 exit()
