f50be75db2a97503cbecf225cb12234669badc96
Jon Reades <jon@reades.com>
2025-09-26 14:25:13 +0100

    Adding looping over any/all/some/one talk easily.

--- flip.py
+++ flip.py
@@ -16,7 +16,7 @@ parser = argparse.ArgumentParser(
                     epilog='For example: `python flip/process.py -t 3.4-Functions -n "It\'s Functional"`')
 parser.add_argument('-p', '--project', type=str, help="Path to the project.toml configuration file.", default='project.toml')
 parser.add_argument('-d', '--defaults', type=str, help="Path to the defaults.toml configuration file.", default='defaults.toml')
-parser.add_argument('-l', '--lesson', type=int, help="Name of the lesson in the project.toml configuration file.", default=1)
+parser.add_argument('-l', '--lesson', type=str, help="The number (or range) of the lesson in the project.toml configuration file. Examples include: `1`, `3-5`, `3,7-8,9`", default='-1')
 parser.add_argument('-i', '--noimage', help="Skip export of the slide deck to PNG (useful when you are mucking about with the audio and output).", action='store_true')
 parser.add_argument('-a', '--noaudio', help="Skip export of the audio files to M4A (useful when you are mucking about with the audio and output).", action='store_true')
 parser.add_argument('-f', '--force', help="Force generation of new images, audio, and video.", action='store_true')
@@ -41,59 +41,94 @@ for k,v in proj.items():
     if k not in conf:
         conf[k] = v
 
-# Extract Slides
-if not args.noimage:
+# Unpack the lesson option
+lesson_list = []
+if args.lesson == '-1':
+    lesson_list = list(range(1, len(proj['lessons'])+1))
+else:   
+    for part in args.lesson.split(','):
+        if '-' in part:
+            start, end = part.split('-')
+            lesson_list.extend(list(range(int(start), int(end)+1)))
+        else:
+            lesson_list.append(int(part))
+    lesson_list = sorted(list(set(lesson_list)))
+print(lesson_list)
+
+# Insert loop here -- need to read in 
+# the list of available lessons and then
+# iterate over them
+for l_num in lesson_list:
+
+    if l_num < 1 or l_num > len(proj['lessons']):
+        print(f"! Lesson {l_num} is out of range. Skipping.")
+        continue
+
+    lesson = proj['lessons'][str(l_num)]
+    
     print("=" * 40)
-    print("=" * 11 + " Extracting deck " + "=" * 12)
+    print(f"= Processing lesson {l_num}: {lesson['title']}")
+    print("=" * 40)
+    
+    # Extract Slides
+    if not args.noimage:
+        print("=" * 40)
+        print("=" * 11 + " Extracting deck " + "=" * 12)
+        print("=" * 40)
+        cmd = ''
+        cmd += f'{ppath / "python"} {"deck.py"} \\\n'
+        cmd += f'  -p {args.project} \\\n'
+        cmd += f'  -l {l_num}'
+
+        if DEBUG:
+            print(f"- Extracting slides with command:")
+            print(f"{cmd}")
+        call(cmd, shell=True)
+    else: 
+        print(f"- Skipping extraction of lesson {l_num}.")
+        print(f"  Add `-i` to force export of slide deck to PNG.")
+
+    # Extract Audio
+    if not args.noaudio:
+        print("=" * 40)
+        print("=" * 11 + " Extracting audio " + "=" * 12)
+        print("=" * 40)
+        cmd = ''
+        cmd += f'{ppath / "python"} {"audio.py"} \\\n'
+        cmd += f'  -p {args.project} \\\n'
+        cmd += f'  -l {l_num}'
+
+        if DEBUG:
+            print(f"- Extracting audio with command:")
+            print(f"{cmd}")
+        call(cmd, shell=True)
+    else: 
+        print(f"- Skipping extraction of lesson {l_num}.")
+        print(f"  Add `-a` to force export of narration to segmented M4A files.")
+
+    # Merge
+    print("=" * 40)
+    print("=" * 15 + " Merging. " + "=" * 15)
     print("=" * 40)
     cmd = ''
-    cmd += f'{ppath / "python"} {"deck.py"} \\\n'
+    cmd += f'{ppath / "python"} {"merge.py"} \\\n'
     cmd += f'  -p {args.project} \\\n'
-    cmd += f'  -l {args.lesson}'
+    cmd += f'  -l {l_num} \\\n'
+    if args.force:
+        cmd += f'  -f \\\n'
+    if args.quick:
+        cmd += f'  -q \\\n'
 
+    # Do we need to check if the file exists or will 
+    # absence of the force command cause the nested
+    # one to return successfully?
     if DEBUG:
-        print(f"- Extracting slides with command:")
+        print(f"- Merging segments with command:")
         print(f"{cmd}")
     call(cmd, shell=True)
-else: 
-    print(f"- Skipping extraction of lesson {args.lesson}.")
-    print(f"  Add `-i` to force export of slide deck to PNG.")
 
-# Extract Audio
-if not args.noaudio:
-    print("=" * 40)
-    print("=" * 11 + " Extracting audio " + "=" * 12)
-    print("=" * 40)
-    cmd = ''
-    cmd += f'{ppath / "python"} {"audio.py"} \\\n'
-    cmd += f'  -p {args.project} \\\n'
-    cmd += f'  -l {args.lesson}'
+    print(f"  + Done processing lesson {l_num}")
+    # End loop here
 
-    if DEBUG:
-        print(f"- Extracting audio with command:")
-        print(f"{cmd}")
-    call(cmd, shell=True)
-else: 
-    print(f"- Skipping extraction of lesson {args.lesson}.")
-    print(f"  Add `-a` to force export of narration to segmented M4A files.")
-
-# Merge
-print("=" * 40)
-print("=" * 15 + " Merging. " + "=" * 15)
-print("=" * 40)
-cmd = ''
-cmd += f'{ppath / "python"} {"merge.py"} \\\n'
-cmd += f'  -p {args.project} \\\n'
-cmd += f'  -l {args.lesson} \\\n'
-if args.force:
-    cmd += f'  -f \\\n'
-if args.quick:
-    cmd += f'  -q \\\n'
-
-if DEBUG:
-    print(f"- Merging segments with command:")
-    print(f"{cmd}")
-call(cmd, shell=True)
-
-print("+ Done.")
+print("+ Done flipping.")
 exit()
